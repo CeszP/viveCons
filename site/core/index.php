3d56ea9ce092c5fabaa735d2591df8c1f8915f53
Cesz <Cesz>
2017-01-24 23:25:04 -0600

    Footer incomplete

--- site/core/index.php
+++ site/core/index.php
@@ -33,7 +33,7 @@
 </head>
 <body>
 	<?php 
-		include ("nav.php");
+		include ("nav.html");
 		include ("slider.html");
 	 ?>
 	<div id="logo">
@@ -63,88 +63,78 @@
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente nihil quae consequuntur beatae, reiciendis vel corrupti odit, sequi accusamus, fuga excepturi id architecto quod recusandae perspiciatis debitis nesciunt eos sint!
 		</div>
 	</div>
-	<script type="text/javascript">	
-			$(function() {
-			
-				var Page = (function() {
+<script type="text/javascript">
+	$(function() {
+		var Page = (function() {
+			var $navArrows = $( '#nav-arrows' ),
+			$nav = $( '#nav-dots > span' ),
+			slitslider = $( '#slider' ).slitslider( {
+				onBeforeChange : function( slide, pos ) {
+					$nav.removeClass( 'nav-dot-current' );
+					$nav.eq( pos ).addClass( 'nav-dot-current' );
 
-					var $navArrows = $( '#nav-arrows' ),
-						$nav = $( '#nav-dots > span' ),
-						slitslider = $( '#slider' ).slitslider( {
-							onBeforeChange : function( slide, pos ) {
+				}
+			} ),
 
-								$nav.removeClass( 'nav-dot-current' );
-								$nav.eq( pos ).addClass( 'nav-dot-current' );
+			init = function() {
+				initEvents();
+			},
+			initEvents = function() {
 
-							}
-						} ),
+				// add navigation events
+				$navArrows.children( ':last' ).on( 'click', function() {
 
-						init = function() {
+					slitslider.next();
+					return false;
+				} );
 
-							initEvents();
-							
-						},
-						initEvents = function() {
+				$navArrows.children( ':first' ).on( 'click', function() {
 
-							// add navigation events
-							$navArrows.children( ':last' ).on( 'click', function() {
+					slitslider.previous();
+					return false;
+				} );
 
-								slitslider.next();
-								return false;
+				$nav.each( function( i ) {
 
-							} );
+					$( this ).on( 'click', function( event ) {
 
-							$navArrows.children( ':first' ).on( 'click', function() {
-								
-								slitslider.previous();
-								return false;
+						var $dot = $( this );
 
-							} );
+						if( !slitslider.isActive() ) {
 
-							$nav.each( function( i ) {
-							
-								$( this ).on( 'click', function( event ) {
-									
-									var $dot = $( this );
-									
-									if( !slitslider.isActive() ) {
+							$nav.removeClass( 'nav-dot-current' );
+							$dot.addClass( 'nav-dot-current' );
+						}
 
-										$nav.removeClass( 'nav-dot-current' );
-										$dot.addClass( 'nav-dot-current' );
-									
-									}
-									
-									slitslider.jump( i + 1 );
-									return false;
-								
-								} );
-								
-							} );
+						slitslider.jump( i + 1 );
+						return false;
+					} );
 
-						};
+				} );
 
-						return { init : init };
+			};
 
-				})();
+		return { init : init };
 
-				Page.init();
+	})();
+	Page.init();
+/**
+ * Notes: 
+ * 
+ * example how to add items:
+ */
 
-				/**
-				 * Notes: 
-				 * 
-				 * example how to add items:
-				 */
+/*
 
-				/*
-				
-				var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
-				
-				// call the plugin's add method
-				ss.add($items);
+var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
 
-				*/
-			
-			});
-		</script>
+// call the plugin's add method
+ss.add($items);
+
+*/
+</script>
+<?php 
+	include('footer.html');
+ ?>
 </body>
 </html>
--- site/core/servicios.php
+++ site/core/servicios.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<?php 
-		include("nav.php");
+		include("nav.html");
 	 ?>
 </body>
 </html>
